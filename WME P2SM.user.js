64fd1602cb08cee58c4ebbaad83d9a5d908f3b53
Iridium1 <70545967+iridium1-waze@users.noreply.github.com>
2024-10-08 21:03:12 +0200

    Update WME P2SM.user.js

--- WME P2SM.user.js
+++ WME P2SM.user.js
@@ -1,18 +1,22 @@
 // ==UserScript==
 // @name        WME Permalink to several Maps
-// @description This script creates buttons to permalink page on several Maps.
+// @description This script creates buttons to open pages of several other maps based on the WME coordinates
 // @namespace   https://github.com/iridium1-waze/WME-P2SM/blob/master/WME%20P2SM.user.js
-// @version     2024.10.04.01
+// @version     2024.10.08.01
 // @match       https://*.waze.com/editor*
 // @match       https://*.waze.com/*/editor*
 // @match       https://beta.waze.com/editor*
 // @match       https://beta.waze.com/*/editor*
 // @icon        https://raw.githubusercontent.com/iridium1-waze/WME-Core-Files/master/map_icon.png
+// @license     MIT
+// @syncURL     https://github.com/iridium1-waze/WME-L2DEGEO/raw/main/WME%20L2DEGEO.user.js
+// @downloadURL https://update.greasyfork.org/scripts/511905/WME%20Permalink%20to%20Several%20Maps.user.js
+// @updateURL   https://update.greasyfork.org/scripts/511905/WME%20Permalink%20to%20Several%20Maps.meta.js
 // @grant       none
 // ==/UserScript==
 
 // Mini howto:
-// 1) install this script as GitHub script
+// 1) install this script as Greasy Fork or GitHub script
 // 2) Click on buttons on the sidebar to open selected map service with coordinates coming from WME
 
 //changes by Iridium1 (contact either PM or iridium1.waze@gmail.com)
@@ -48,6 +52,7 @@
 //2024.04.07.01: Typo in version number
 //2024.05.17.01: Quick-Fix for ViaMichelin - URL no longer working. ToDo: Work on zoom settings
 //2024.10.04.01: Added Lookmap
+//2024.10.08.01: Added webhook for Greasy Fork - thankd to Dancingman81!
 
 /* global W */
 /* global proj4 */
@@ -58,7 +63,7 @@
 
 // indicate used variables to be assigned
 
-var p2sm_version = "2024.10.04.01";
+var p2sm_version = "2024.10.08.01";
 
 function getCenterZoom() {
     var map = W.map.getOLMap()
